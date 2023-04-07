0a51aa26cbd501c2b1ff906b2be9d20e3e509906
Benyamain <yacoobbenyamain@gmail.com>
2023-04-07 16:16:00 -0400

    Did all the MySQL queries for our Reimbursement Database. Time to see it in action and store the input fields!

--- app.js
+++ app.js
@@ -34,14 +34,68 @@ const connection = mysql.createConnection({
   user: "root",
   port: 3306,
   password: "1234",
+  database: "reimbursement_db"
 });
 
 connection.connect();
 
-connection.query("SELECT 1 + 1 AS solution", (err, rows, fields) => {
+connection.query("CREATE TABLE IF NOT EXISTS Faculty (\n employmentNumber int NOT NULL,\n"
++ "fName varchar(45) NOT NULL,\n"
++ "lName varchar(45) NOT NULL,\n"
++ "workEmail varchar(45) NOT NULL,\n"
++ "phoneNumber varchar(45) NOT NULL,\n"
++ "password varchar(45) NOT NULL,\n"
++ "mailingAddress varchar(45) NOT NULL,\n"
++ "department varchar(45) NOT NULL,\n"
++ "zipCode int NOT NULL,\n"
++ "city varchar(45) NOT NULL,\n"
++ "state varchar(2) NOT NULL,\n"
++ "PRIMARY KEY (employmentNumber));", (err, rows, fields) => {
   if (err) console.log(err);
+});
 
-  console.log("The solution is: ", rows[0].solution);
+connection.query("CREATE TABLE IF NOT EXISTS FOAPA (\n foapaName varchar(45) NOT NULL,\n"
++ "foapaNumber int NOT NULL,\n"
++ "PRIMARY KEY (foapaNumber));", (err, rows, fields) => {
+  if (err) console.log(err);
+});
+
+connection.query("CREATE TABLE IF NOT EXISTS Possesses (\n employmentNumber int NOT NULL,\n"
++ "foapaNumber int NOT NULL,\n"
++ "PRIMARY KEY (employmentNumber, foapaNumber),\n"
++ "FOREIGN KEY (employmentNumber) REFERENCES Faculty(employmentNumber),\n"
++ "FOREIGN KEY (foapaNumber) REFERENCES FOAPA(foapaNumber));", (err, rows, fields) => {
+  if (err) console.log(err);
+});
+
+connection.query("CREATE TABLE IF NOT EXISTS Activity (\n activityId int NOT NULL,\n"
++ "foapaNumber int NOT NULL,\n"
++ "activityName varchar(45) NOT NULL,\n"
++ "activityReceipt varchar(45) NOT NULL,\n"
++ "activityDate date NOT NULL,\n"
++ "amount float NOT NULL,\n"
++ "PRIMARY KEY (activityId),\n"
++ "FOREIGN KEY (foapaNumber) REFERENCES FOAPA(foapaNumber));", (err, rows, fields) => {
+  if (err) console.log(err);
+});
+
+connection.query("CREATE TABLE IF NOT EXISTS ReimbursementTicket (\n reimbursementId int NOT NULL,\n"
++ "employmentNumber int NOT NULL,\n"
++ "eventName varchar(45) NOT NULL,\n"
++ "totalAmount float NOT NULL,\n"
++ "reimbursementStatus bool NOT NULL,\n"
++ "reimbursementDate date NOT NULL,\n"
++ "PRIMARY KEY (reimbursementId),\n"
++ "FOREIGN KEY (employmentNumber) REFERENCES Faculty(employmentNumber));", (err, rows, fields) => {
+  if (err) console.log(err);
+});
+
+connection.query("CREATE TABLE IF NOT EXISTS Contains (\n reimbursementId int NOT NULL,\n"
++ "activityId int NOT NULL,\n"
++ "PRIMARY KEY (reimbursementId, activityId),\n"
++ "FOREIGN KEY (reimbursementId) REFERENCES ReimbursementTicket(reimbursementId),\n"
++ "FOREIGN KEY (activityId) REFERENCES Activity(activityId));", (err, rows, fields) => {
+  if (err) console.log(err);
 });
 
 connection.end();
