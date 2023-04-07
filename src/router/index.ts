ffa782e5cfc2d6a559d6903b1b2b9742077e3b83
Benyamain <yacoobbenyamain@gmail.com>
2023-04-07 17:50:14 -0400

    This branch works on setting up the rerouting of pages. Did not actually implement the code but did create all the necessary files to make the rerouting possible

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import SignupPage from "../pages/signup_page.vue";
 import LoginPage from "../pages/login_page.vue";
 import AccountPage from "../pages/account_page.vue";
+import DashboardPage from "../pages/dashboard_page.vue";
+import ReimbursementPage from "../pages/reimbursement_page.vue";
+
 export default createRouter({
   history: createWebHistory(),
   routes: [
@@ -16,6 +19,14 @@ export default createRouter({
     {
       path: "/account",
       component: AccountPage,
+    },
+    {
+      path: "/dashboard",
+      component: DashboardPage,
+    },
+    {
+      path: "/reimbursement",
+      component: ReimbursementPage,
     },
   ],
 });
