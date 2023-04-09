549a9a48d3655102b8fc382c81b60a93c881d6a7
Ara <oladipoeyiara@gmail.com>
2023-04-09 19:50:48 -0400

    Adding random code that hopefully should stop weird errors

--- app.js
+++ app.js
@@ -112,3 +112,12 @@ app.post("/api/register", (req, res) => {
 app.get("/close", () => {
   connection.end();
 });
+
+process
+  .on("unhandledRejection", (reason, p) => {
+    console.error(reason, "Unhandled Rejection at Promise", p);
+  })
+  .on("uncaughtException", (err) => {
+    console.error(err, "Uncaught Exception thrown");
+    process.exit(1);
+  });
