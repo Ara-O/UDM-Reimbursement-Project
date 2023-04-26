786987247a1cff90b4cf00e80d975149e07364c1
Benyamain <yacoobbenyamain@gmail.com>
2023-04-26 00:30:21 -0400

    Almost broke the code. Last commit did that. This commit fixed it. Phew

--- src/pages/signup_page.vue
+++ src/pages/signup_page.vue
@@ -1,261 +1,316 @@
 <template>
   <section class="signup-page">
-    <!-- Img is a way you can embed images to your site,
-    the ../ before the folder means that we are traversing file systems -->
-    <div class="udmercy-logo-wrapper">
-      <img
-        src="../assets/detroit-mercy-logo.png"
-        alt="Detroit mercy logo"
-        class="udmercy-logo"
-      />
-    </div>
-    <br />
-    <h3 class="signup-title">Detroit Mercy Reimbursement System</h3>
-    <br />
-    <form @submit.prevent="registerUser" class="signup-form">
-      <div class="input-field">
-        <label for="first-name">First Name: </label>
-        <input
-          type="text"
-          name="First name"
-          id="first-name"
-          v-model="userSignupData.firstName"
-          required
+    <section class="left-section">
+      <div class="udmercy-logo-wrapper">
+        <img
+          src="../assets/detroit-mercy-logo.png"
+          alt="Detroit mercy logo"
+          class="udmercy-logo"
         />
       </div>
-      <div class="input-field">
-        <label for="last-name">Last Name: </label>
-        <input
-          type="text"
-          name="Last Name"
-          id="last-name"
-          v-model="userSignupData.lastName"
-          required
+    </section>
+    <section class="right-section">
+      <div class="udmercy-logo-wrapper-mobile">
+        <img
+          src="../assets/detroit-mercy-logo.png"
+          alt="Detroit mercy logo"
+          class="udmercy-logo-mobile"
         />
       </div>
-      <div class="input-field">
-        <label for="work-email">Work Email: </label>
-        <input
-          type="email"
-          name="Work Email"
-          id="work-email"
-          v-model="userSignupData.workEmail"
-          required
-        />
-      </div>
-      <div class="input-field">
-        <label for="employment-number">Employment Number: </label>
-        <input
-          type="number"
-          name="Employment Number"
-          id="employment-number"
-          v-model="userSignupData.employmentNumber"
-          required
-        />
-      </div>
-      <div class="input-field">
-        <label for="department">Department:</label>
-        <input
-          type="text"
-          name="Department"
-          id="department"
-          v-model="userSignupData.department"
-          required
-        />
-      </div>
-      <div class="input-field">
-        <label for="phone-number">Phone Number:</label>
-        <input
-          type="text"
-          name="Phone Number"
-          id="phone-number"
-          v-model="userSignupData.phoneNumber"
-          required
-        />
-      </div>
-      <div class="input-field">
-        <label for="password">Password:</label>
-        <input
-          type="password"
-          name="Password"
-          id="password"
-          v-model="userSignupData.password"
-          required
-        />
-      </div>
-      <div class="input-field">
-        <label for="reenter-password">Re-enter password:</label>
-        <input
-          type="password"
-          name="reenter-password"
-          id="reenter-password"
-          v-model="reEnteredPassword"
-          required
-        />
-      </div>
-      <!-- Store foapa numbers in their own table -->
-      <div class="input-field">
-        <label for="street-address">Street Address:</label>
-        <input
-          type="text"
-          name="Street Address"
-          id="street-address"
-          v-model="userSignupData.mailingAddress"
-          required
-        />
-      </div>
-      <div class="input-field">
-        <label for="city">City:</label>
-        <input
-          type="text"
-          name="City"
-          id="city"
-          v-model="userSignupData.city"
-          required
-        />
-      </div>
-      <div class="input-field">
-        <label for="state">State:</label>
-        <input
-          list="states"
-          type="text"
-          name="State"
-          id="state"
-          v-model="userSignupData.state"
-          required
-        />
+      <h3 class="signup-title">Detroit Mercy Reimbursement System</h3>
 
-        <datalist id="states">
-          <option :value="state" v-for="state in states">
-            {{ state }}
-          </option>
-        </datalist>
-      </div>
-      <div class="input-field">
-        <label for="zip-code">Zip Code:</label>
-        <input
-          type="number"
-          name="Zip Code"
-          id="zip-code"
-          v-model="userSignupData.zipCode"
-          required
-        />
-      </div>
-      <div class="input-field">
-        <label for="country">Country:</label>
-        <input
-          list="countries"
-          type="text"
-          name="Country"
-          id="country"
-          v-model="userSignupData.country"
-          required
-        />
+      <form @submit.prevent="registerUser" class="signup-form">
+        <section v-show="surveyProgress === 0" class="signup-form">
+          <div class="input-field-wrapper">
+            <div class="input-field">
+              <label for="first-name">First Name:*</label>
+              <input
+                type="text"
+                name="First name"
+                id="first-name"
+                v-model="userSignupData.firstName"
+                required
+              />
+            </div>
+            <div class="input-field">
+              <label for="last-name">Last Name:*</label>
+              <input
+                type="text"
+                name="Last Name"
+                id="last-name"
+                v-model="userSignupData.lastName"
+                required
+              />
+            </div>
+            <div class="input-field">
+              <label for="work-email">Work Email: *</label>
+              <input
+                type="email"
+                name="Work Email"
+                id="work-email"
+                placeholder="@udmercy.edu"
+                v-model="userSignupData.workEmail"
+                required
+              />
+            </div>
+          </div>
+          <div class="input-field-wrapper">
+            <div class="input-field">
+              <label for="employment-number">Employment Number: *</label>
+              <input
+                type="number"
+                name="Employment Number"
+                style="width: 90px"
+                id="employment-number"
+                v-model="userSignupData.employmentNumber"
+                required
+              />
+            </div>
+            <div class="input-field">
+              <label for="phone-number">Phone: *</label>
+              <input
+                type="text"
+                name="Phone Number"
+                id="phone-number"
+                v-model="userSignupData.phoneNumber"
+                required
+              />
+            </div>
+            <div class="input-field">
+              <label for="street-address">Street Address: *</label>
+              <input
+                type="text"
+                name="Street Address"
+                id="street-address"
+                v-model="userSignupData.mailingAddress"
+                required
+              />
+            </div>
+          </div>
+          <div class="input-field-wrapper">
+            <div class="input-field">
+              <label for="department">Department: *</label>
+              <input
+                type="text"
+                name="Department"
+                id="department"
+                v-model="userSignupData.department"
+                required
+              />
+            </div>
+            <div class="input-field">
+              <label for="password">Password: *</label>
+              <input
+                type="password"
+                name="Password"
+                id="password"
+                v-model="userSignupData.password"
+                required
+              />
+            </div>
+            <div class="input-field">
+              <label for="reenter-password">Re-enter password: *</label>
+              <input
+                type="password"
+                name="reenter-password"
+                id="reenter-password"
+                v-model="reEnteredPassword"
+                required
+              />
+            </div>
+          </div>
+          <button
+            class="signup-button"
+            type="button"
+            @click="surveyProgress = 1"
+            style="margin-top: 0px"
+          >
+            Continue
+          </button>
+        </section>
 
-        <datalist id="countries">
-          <option :value="country" v-for="country in countries">
-            {{ country }}
-          </option>
-        </datalist>
-      </div>
-      <div class="input-field">
-        <label for="foapa-numbers">FOAPA:</label>
-      </div>
-      <span
-        class="input-FOAPA-field"
-        style="display: inline-flexbox; gap: 10px"
-      >
-        <input
-          type="text"
-          placeholder="FOAPA Name"
-          name="FOAPA Name"
-          id="foapa-name"
-          v-model="userFoapaStuff.foapaName"
-          style="width: 171px; margin-left: -189px"
-        />
-        <div class="input-FOAPA-field">
-          <input
-            type="text"
-            name="F input"
-            id="f-input"
-            placeholder="xxxx"
-            v-model="userFoapaStuff.fNumber"
-            style="margin-left: 0px"
-          />
-        </div>
-        -
-        <div class="input-FOAPA-field">
-          <input
-            type="text"
-            name="O input"
-            id="o-input"
-            placeholder="xxxx"
-            v-model="userFoapaStuff.oNumber"
-            style="margin-left: 0px"
-          />
-        </div>
-        -
-        <div class="input-FOAPA-field">
-          <input
-            type="text"
-            name="A input"
-            id="a-input"
-            placeholder="xxxx"
-            v-model="userFoapaStuff.aNumber"
-            style="margin-left: 0px"
-          />
-        </div>
-        -
-        <div class="input-FOAPA-field">
-          <input
-            type="text"
-            name="P input"
-            id="p-input"
-            placeholder="xxxx"
-            v-model="userFoapaStuff.pNumber"
-            style="margin-left: 0px"
-          />
-        </div>
-        -
-        <div class="input-FOAPA-field">
-          <input
-            type="text"
-            name="A2 input"
-            id="a2-input"
-            placeholder="xxxx"
-            v-model="userFoapaStuff.a2Number"
-            style="margin-left: 0px"
-          />
-        </div>
-        <button class="add-foapa-button" type="button" @click="addFoapa">
-          <img
-            src="../assets/add-icon.png"
-            alt="add-icon"
-            style="width: 15px"
-          />
-        </button>
-      </span>
-      <div v-for="foapa in foapaList" style="display: inline-flex">
-        <h3>{{ foapa.foapaName }} : {{ foapa.fNumber }}</h3>
-        <h3>-</h3>
-        <h3>{{ foapa.oNumber }}</h3>
-        <h3>-</h3>
-        <h3>{{ foapa.aNumber }}</h3>
-        <h3>-</h3>
-        <h3>{{ foapa.pNumber }}</h3>
-        <h3>-</h3>
-        <h3>{{ foapa.a2Number }}</h3>
-      </div>
-      <br /><br />
-      <button class="signup-button" type="submit" style="margin-top: 0px">
-        Continue
-      </button>
-      <br />
-      <router-link to="/" style="font-size: 14px"
-        >Already have an Account</router-link
-      >
-      <br />
-    </form>
+        <!-- SECTION 2 -->
+        <section v-show="surveyProgress === 1" class="signup-form">
+          <div class="input-field-wrapper">
+            <div class="input-field">
+              <label for="city">City: *</label>
+              <input
+                type="text"
+                name="City"
+                id="city"
+                v-model="userSignupData.city"
+                required
+              />
+            </div>
+            <div class="input-field">
+              <label for="state">State: *</label>
+              <input
+                list="states"
+                type="text"
+                name="State"
+                id="state"
+                v-model="userSignupData.state"
+                required
+              />
+
+              <datalist id="states">
+                <option :value="state" v-for="state in states">
+                  {{ state }}
+                </option>
+              </datalist>
+            </div>
+          </div>
+          <div class="input-field-wrapper">
+            <div class="input-field">
+              <label for="zip-code">Zip Code: *</label>
+              <input
+                type="number"
+                name="Zip Code"
+                id="zip-code"
+                v-model="userSignupData.zipCode"
+                required
+              />
+            </div>
+            <div class="input-field">
+              <label for="country">Country: *</label>
+              <input
+                list="countries"
+                type="text"
+                name="Country"
+                id="country"
+                v-model="userSignupData.country"
+                required
+              />
+
+              <datalist id="countries">
+                <option :value="country" v-for="country in countries">
+                  {{ country }}
+                </option>
+              </datalist>
+            </div>
+          </div>
+          <div class="foapa-section">
+            <div>
+              <label for="foapa-numbers"
+                >FOAPA [ FUND - ORG - ACCT - PROG - ACTV ]:</label
+              >
+            </div>
+            <br />
+            <div class="input-field-foapa-wrapper">
+              <div class="input-FOAPA-field">
+                <input
+                  type="text"
+                  style="width: 150px"
+                  placeholder="FOAPA Name"
+                  name="FOAPA Name"
+                  id="foapa-name"
+                  v-model="userFoapaStuff.foapaName"
+                />
+              </div>
+              :
+              <div class="input-FOAPA-field">
+                <input
+                  type="text"
+                  name="F input"
+                  id="f-input"
+                  placeholder="xxxxxx"
+                  v-model="userFoapaStuff.fNumber"
+                />
+              </div>
+              -
+              <div class="input-FOAPA-field">
+                <input
+                  type="text"
+                  name="O input"
+                  id="o-input"
+                  placeholder="xxxx"
+                  v-model="userFoapaStuff.oNumber"
+                />
+              </div>
+              -
+              <div class="input-FOAPA-field">
+                <input
+                  type="text"
+                  name="A input"
+                  id="a-input"
+                  placeholder="xxxx"
+                  v-model="userFoapaStuff.aNumber"
+                />
+              </div>
+              -
+              <div class="input-FOAPA-field">
+                <input
+                  type="text"
+                  name="P input"
+                  id="p-input"
+                  placeholder="xxxx"
+                  v-model="userFoapaStuff.pNumber"
+                />
+              </div>
+              -
+              <div class="input-FOAPA-field">
+                <input
+                  type="text"
+                  name="A2 input"
+                  id="a2-input"
+                  placeholder="xxxx"
+                  v-model="userFoapaStuff.a2Number"
+                />
+              </div>
+              <button class="add-foapa-button" type="button" @click="addFoapa">
+                <img
+                  src="../assets/add-icon.png"
+                  alt="add-icon"
+                  style="width: 15px"
+                />
+              </button>
+            </div>
+            <br />
+            <div
+              v-for="(foapa, index) in foapaList"
+              style="display: flex"
+              class="added-foapa-number"
+            >
+              <h3 style="margin-right: 15px; font-weight: 500">
+                #{{ index + 1 }}
+              </h3>
+              <h3>{{ foapa.foapaName }} : {{ foapa.fNumber }}</h3>
+              <h3>-</h3>
+              <h3>{{ foapa.oNumber }}</h3>
+              <h3>-</h3>
+              <h3>{{ foapa.aNumber }}</h3>
+              <h3>-</h3>
+              <h3>{{ foapa.pNumber }}</h3>
+              <h3>-</h3>
+              <h3>{{ foapa.a2Number }}</h3>
+            </div>
+          </div>
+          <div class="continue-buttons">
+            <button
+              class="signup-button"
+              type="button"
+              @click="surveyProgress = 0"
+              style="margin-top: 0px"
+            >
+              Go Back
+            </button>
+            <button class="signup-button" type="submit" style="margin-top: 0px">
+              Register
+            </button>
+          </div>
+        </section>
+
+        <router-link
+          to="/"
+          style="font-size: 14px; margin-top: -15px"
+          class="already-has-account"
+          >Already have an Account</router-link
+        >
+        <h5 style="font-weight: 300; margin: 0px; margin-top: -25px">
+          Note: All required fields must be filled
+        </h5>
+      </form>
+    </section>
   </section>
 </template>
 
@@ -290,7 +345,7 @@ type FoapaStuff = {
 };
 
 let reEnteredPassword = ref<string>("");
-
+let surveyProgress = ref<number>(0);
 let foapaList = ref<FoapaStuff[]>([]);
 
 const router = useRouter();
@@ -345,7 +400,7 @@ const states = [
   "WA",
   "WV",
   "WI",
-  "WY"
+  "WY",
 ];
 
 const countries = [
@@ -536,7 +591,7 @@ const countries = [
   "Ukraine",
   "United Arab Emirates",
   "United Kingdom",
-  "United States",
+  "United States of America",
   "Uruguay",
   "Uzbekistan",
   "Vanuatu",
@@ -544,7 +599,7 @@ const countries = [
   "Vietnam",
   "Yemen",
   "Zambia",
-  "Zimbabwe"
+  "Zimbabwe",
 ];
 
 let userSignupData = reactive<UserData>({
