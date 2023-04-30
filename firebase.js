f4bab0d45a200d93a88edfc54f2508427f783ff5
Benyamain <yacoobbenyamain@gmail.com>
2023-04-29 23:55:36 -0400

    Forgot to add web auth as well

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -25,3 +26,6 @@ const analytics = getAnalytics(app);
 
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
