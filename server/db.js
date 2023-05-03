53523cae050391cfa7cb059d2c78987d8952e07d
Ara <oladipoeyiara@gmail.com>
2023-05-03 16:25:10 -0400

    Deploying through planetscale and not local mysql

--- server/db.js
+++ server/db.js
@@ -10,14 +10,14 @@ import {
 
 //Related to connections with mysql database
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  port: 3306,
-  password: "password",
-  database: "reimbursement_db",
-});
-// const connection = mysql.createConnection(process.env.DATABASE_URL);
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   port: 3306,
+//   password: "password",
+//   database: "reimbursement_db",
+// });
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect(function (err) {
   if (err) {
