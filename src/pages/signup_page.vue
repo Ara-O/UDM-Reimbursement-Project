c234a4475d38e9c33532e59bdd4e6d764b62825d
Benyamain <yacoobbenyamain@gmail.com>
2023-05-14 22:53:40 -0400

    Revised depts to match the College of Engineering and Science

--- src/pages/signup_page.vue
+++ src/pages/signup_page.vue
@@ -2,20 +2,12 @@
   <section class="signup-page">
     <section class="left-section">
       <div class="udmercy-logo-wrapper">
-        <img
-          src="../assets/detroit-mercy-logo.png"
-          alt="Detroit mercy logo"
-          class="udmercy-logo"
-        />
+        <img src="../assets/detroit-mercy-logo.png" alt="Detroit mercy logo" class="udmercy-logo" />
       </div>
     </section>
     <section class="right-section">
       <div class="udmercy-logo-wrapper-mobile">
-        <img
-          src="../assets/detroit-mercy-logo.png"
-          alt="Detroit mercy logo"
-          class="udmercy-logo-mobile"
-        />
+        <img src="../assets/detroit-mercy-logo.png" alt="Detroit mercy logo" class="udmercy-logo-mobile" />
       </div>
       <h3 class="signup-title">Detroit Mercy Reimbursement System</h3>
       <h3 class="signup-title-description" v-if="surveyProgress === 0">
@@ -33,77 +25,44 @@
           <div class="input-field-wrapper">
             <div class="input-field">
               <label for="first-name">First Name:*</label>
-              <input
-                type="text"
-                name="First name"
-                id="first-name"
-                v-model="userSignupData.firstName"
-              />
+              <input type="text" name="First name" id="first-name" v-model="userSignupData.firstName" />
             </div>
             <div class="input-field">
               <label for="last-name">Last Name:*</label>
-              <input
-                type="text"
-                name="Last Name"
-                id="last-name"
-                v-model="userSignupData.lastName"
-              />
+              <input type="text" name="Last Name" id="last-name" v-model="userSignupData.lastName" />
             </div>
             <div class="input-field">
               <label for="work-email">Work Email: *</label>
-              <input
-                type="email"
-                name="Work Email"
-                id="work-email"
-                v-model="userSignupData.workEmail"
-              />
+              <input type="email" name="Work Email" id="work-email" v-model="userSignupData.workEmail" />
             </div>
           </div>
           <div class="input-field-wrapper">
             <div class="input-field">
               <label for="employment-number">Employment Number: *</label>
               <span style="position: relative">
-                <span
-                  style="
+                <span style="
                     position: absolute;
                     top: 50%;
                     left: 50%;
                     transform: translate(-51px, -50%);
-                  "
-                  ><h3
-                    style="font-size: 14px; font-weight: 400; color: #474747"
-                  >
+                  ">
+                  <h3 style="font-size: 14px; font-weight: 400; color: #474747">
                     T
-                  </h3></span
-                >
-                <input
-                  type="text"
-                  name="Employment Number"
-                  style="width: 130px; padding-left: 25px"
-                  id="employment-number"
-                  v-model="userSignupData.employmentNumber"
-                />
+                  </h3>
+                </span>
+                <input type="text" name="Employment Number" style="width: 130px; padding-left: 25px"
+                  id="employment-number" v-model="userSignupData.employmentNumber" />
               </span>
             </div>
             <div class="input-field">
               <label for="phone-number">Phone: *</label>
-              <input
-                type="text"
-                name="Phone Number"
-                id="phone-number"
-                v-model="userSignupData.phoneNumber"
-              />
+              <input type="text" name="Phone Number" id="phone-number" v-model="userSignupData.phoneNumber" />
             </div>
 
             <div class="input-field">
               <label for="department">Department: *</label>
-              <input
-                list="departmentList"
-                type="text"
-                name="Department"
-                id="department"
-                v-model="userSignupData.department"
-              />
+              <input list="departmentList" type="text" name="Department" id="department"
+                v-model="userSignupData.department" />
               <datalist id="departmentList">
                 <option :value="department" v-for="department in departments">
                   {{ department }}
@@ -114,29 +73,14 @@
           <div class="input-field-wrapper">
             <div class="input-field">
               <label for="password">Password: *</label>
-              <input
-                type="password"
-                name="Password"
-                id="password"
-                v-model="userSignupData.password"
-              />
+              <input type="password" name="Password" id="password" v-model="userSignupData.password" />
             </div>
             <div class="input-field">
               <label for="reenter-password">Re-enter password: *</label>
-              <input
-                type="password"
-                name="reenter-password"
-                id="reenter-password"
-                v-model="reEnteredPassword"
-              />
+              <input type="password" name="reenter-password" id="reenter-password" v-model="reEnteredPassword" />
             </div>
           </div>
-          <button
-            class="signup-button"
-            type="button"
-            @click="finishedBasicQuestionsSection"
-            style="margin-top: 0px"
-          >
+          <button class="signup-button" type="button" @click="finishedBasicQuestionsSection" style="margin-top: 0px">
             Continue
           </button>
         </section>
@@ -146,31 +90,15 @@
           <div class="input-field-wrapper">
             <div class="input-field">
               <label for="street-address">Street Address: *</label>
-              <input
-                type="text"
-                name="Street Address"
-                id="street-address"
-                v-model="userSignupData.mailingAddress"
-              />
+              <input type="text" name="Street Address" id="street-address" v-model="userSignupData.mailingAddress" />
             </div>
             <div class="input-field">
               <label for="city">City: *</label>
-              <input
-                type="text"
-                name="City"
-                id="city"
-                v-model="userSignupData.city"
-              />
+              <input type="text" name="City" id="city" v-model="userSignupData.city" />
             </div>
             <div class="input-field">
               <label for="state">State: *</label>
-              <input
-                list="states"
-                type="text"
-                name="State"
-                id="state"
-                v-model="userSignupData.state"
-              />
+              <input list="states" type="text" name="State" id="state" v-model="userSignupData.state" />
 
               <datalist id="states">
                 <option :value="state" v-for="state in states">
@@ -182,22 +110,11 @@
           <div class="input-field-wrapper">
             <div class="input-field">
               <label for="zip-code">Postal Code: *</label>
-              <input
-                type="number"
-                name="Postal Code"
-                id="zip-code"
-                v-model="userSignupData.zipCode"
-              />
+              <input type="number" name="Postal Code" id="zip-code" v-model="userSignupData.zipCode" />
             </div>
             <div class="input-field">
               <label for="country">Country: *</label>
-              <input
-                list="countries"
-                type="text"
-                name="Country"
-                id="country"
-                v-model="userSignupData.country"
-              />
+              <input list="countries" type="text" name="Country" id="country" v-model="userSignupData.country" />
 
               <datalist id="countries">
                 <option :value="country" v-for="country in countries">
@@ -208,20 +125,10 @@
           </div>
 
           <div class="continue-buttons">
-            <button
-              class="signup-button"
-              type="button"
-              @click="surveyProgress = 0"
-              style="margin-top: 0px"
-            >
+            <button class="signup-button" type="button" @click="surveyProgress = 0" style="margin-top: 0px">
               Go Back
             </button>
-            <button
-              class="signup-button"
-              type="button"
-              style="margin-top: 0px"
-              @click="finishedAddressSection"
-            >
+            <button class="signup-button" type="button" style="margin-top: 0px" @click="finishedAddressSection">
               Continue
             </button>
           </div>
@@ -231,100 +138,54 @@
         <section v-show="surveyProgress === 2" class="signup-form">
           <div>
             <div>
-              <label for="foapa-numbers"
-                >FOAPA [ FUND - ORG - ACCT - PROG - ACTV ]:</label
-              >
+              <label for="foapa-numbers">FOAPA [ FUND - ORG - ACCT - PROG - ACTV ]:</label>
             </div>
             <br />
             <div class="input-field-foapa-wrapper">
               <div class="input-FOAPA-field">
                 <label for="foapa-name">FOAPA Name</label>
-                <input
-                  type="text"
-                  style="width: 150px"
-                  placeholder="FOAPA Name"
-                  name="FOAPA Name"
-                  id="foapa-name"
-                  v-model="userFoapaStuff.foapaName"
-                />
+                <input type="text" style="width: 150px" placeholder="FOAPA Name" name="FOAPA Name" id="foapa-name"
+                  v-model="userFoapaStuff.foapaName" />
               </div>
               <h5>-</h5>
 
               <div class="input-FOAPA-field">
                 <label for="foapa-name">FUND</label>
 
-                <input
-                  type="text"
-                  name="F input"
-                  id="f-input"
-                  placeholder="xxxxxx"
-                  v-model="userFoapaStuff.fNumber"
-                />
+                <input type="text" name="F input" id="f-input" placeholder="xxxxxx" v-model="userFoapaStuff.fNumber" />
               </div>
               <h5>-</h5>
 
               <div class="input-FOAPA-field">
                 <label for="foapa-name">ORG</label>
 
-                <input
-                  type="text"
-                  name="O input"
-                  id="o-input"
-                  placeholder="xxxx"
-                  v-model="userFoapaStuff.oNumber"
-                />
+                <input type="text" name="O input" id="o-input" placeholder="xxxx" v-model="userFoapaStuff.oNumber" />
               </div>
               <h5>-</h5>
               <div class="input-FOAPA-field">
                 <label for="foapa-name">ACCT</label>
 
-                <input
-                  type="text"
-                  name="A input"
-                  id="a-input"
-                  placeholder="xxxx"
-                  v-model="userFoapaStuff.aNumber"
-                />
+                <input type="text" name="A input" id="a-input" placeholder="xxxx" v-model="userFoapaStuff.aNumber" />
               </div>
               <h5>-</h5>
 
               <div class="input-FOAPA-field">
                 <label for="foapa-name">PROG</label>
 
-                <input
-                  type="text"
-                  name="P input"
-                  id="p-input"
-                  placeholder="xxxx"
-                  v-model="userFoapaStuff.pNumber"
-                />
+                <input type="text" name="P input" id="p-input" placeholder="xxxx" v-model="userFoapaStuff.pNumber" />
               </div>
               <h5>-</h5>
               <div class="input-FOAPA-field">
                 <label for="foapa-name">ACTV</label>
 
-                <input
-                  type="text"
-                  name="A2 input"
-                  id="a2-input"
-                  placeholder="xxxx"
-                  v-model="userFoapaStuff.a2Number"
-                />
+                <input type="text" name="A2 input" id="a2-input" placeholder="xxxx" v-model="userFoapaStuff.a2Number" />
               </div>
               <button class="add-foapa-button" type="button" @click="addFoapa">
-                <img
-                  src="../assets/add-icon.png"
-                  alt="add-icon"
-                  style="width: 15px"
-                />
+                <img src="../assets/add-icon.png" alt="add-icon" style="width: 15px" />
               </button>
             </div>
             <br />
-            <div
-              v-for="(foapa, index) in foapaList"
-              style="display: flex"
-              class="added-foapa-number"
-            >
+            <div v-for="(foapa, index) in foapaList" style="display: flex" class="added-foapa-number">
               <h3 style="margin-right: 15px; font-weight: 500">
                 #{{ index + 1 }}
               </h3>
@@ -339,32 +200,18 @@
               <h3>{{ foapa.a2Number }}</h3>
             </div>
             <div class="continue-buttons" style="margin-top: 20px">
-              <button
-                class="signup-button"
-                type="button"
-                @click="surveyProgress--"
-                style="margin-top: 0px"
-              >
+              <button class="signup-button" type="button" @click="surveyProgress--" style="margin-top: 0px">
                 Go Back
               </button>
-              <button
-                class="signup-button"
-                @click="registerUser"
-                style="margin-top: 0px"
-              >
+              <button class="signup-button" @click="registerUser" style="margin-top: 0px">
                 Create Account
               </button>
             </div>
           </div>
         </section>
 
-        <router-link to="/" style="font-size: 14px; margin-top: -15px"
-          >Already have an Account</router-link
-        >
-        <h5
-          class="required-field-note"
-          style="font-weight: 300; margin-top: -25px"
-        >
+        <router-link to="/" style="font-size: 14px; margin-top: -15px">Already have an Account</router-link>
+        <h5 class="required-field-note" style="font-weight: 300; margin-top: -25px">
           Note: All required fields must be filled
         </h5>
       </section>
@@ -661,33 +508,18 @@ const countries = [
 ];
 
 const departments = [
+  "Architectural Engineering",
+  "Biochemistry",
+  "Civil Engineering",
   "Computer Science",
   "Electrical Engineering",
-  "Mechanical Engineering",
-  "Civil Engineering",
-  "Chemical Engineering",
-  "Biology",
-  "Chemistry",
-  "Physics",
+  "Environmental Engineering",
   "Mathematics",
-  "Statistics",
-  "Psychology",
-  "Sociology",
-  "Anthropology",
-  "History",
-  "Philosophy",
-  "English",
-  "Foreign Languages",
-  "Business",
-  "Economics",
-  "Political Science",
-  "Law",
-  "Education",
-  "Journalism",
-  "Fine Arts",
-  "Music",
-  "Theater",
-  "Architecture",
+  "Mechanical Engineering",
+  "Office of the Dean",
+  "Physics",
+  "Chemistry",
+  "Robotics and Mechatronic Systems Engineering"
 ];
 
 let userSignupData = reactive<UserData>({
@@ -754,7 +586,7 @@ function addFoapa() {
         userFoapaStuff.aNumber =
         userFoapaStuff.pNumber =
         userFoapaStuff.a2Number =
-          "";
+        "";
     }
   }
 }
