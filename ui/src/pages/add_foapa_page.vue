46c83e2ba0c78d7f8120cf256cd7005441dd6a25
Ara-O <oladipoeyiara@gmail.com>
2024-06-23 11:59:37 -0400

    Working on new foapa ui

--- ui/src/pages/add_foapa_page.vue
+++ ui/src/pages/add_foapa_page.vue
@@ -1,19 +1,104 @@
 <template>
-  <section class="add-foapa-page">
-    <!-- <img src="../assets/detroit-mercy-logo.png" class="udmercy-logo" @click="$router.push('/dashboard')" -->
-    <!-- alt="Detroit mercy logo" /> -->
-    <div class="manage-foapa-div">
-      <h3 class="manage-foapa-text">Manage FOAPA</h3>
-      <ManageFoapaDetails :foapa-details="foapaDetails" @changes-were-made="changesWereMade" />
-      <div class="add-foapa-button-wrapper">
-        <button class="add-foapa-button" style="width: auto; padding: 0px 30px" @click="$router.push('/dashboard')">
-          Go to Dashboard
-        </button>
-        <button class="add-foapa-button" @click="saveFOAPA">
-          Save FOAPAs
-        </button>
-      </div>
+  <section class="px-20 pt-10">
+    <div class="flex items-center gap-4 cursor-pointer" @click="returnToDashboard">
+      <img src="../assets/left-arrow.png" alt="Left arrow" class="w-4">
+      <h4 class="font-normal text-sm text-gray-600">Return to dashboard</h4>
     </div>
+    <section class="flex gap-28">
+      <section>
+        <h3 class="mt-28 font-semibold text-3xl">Manage FOAPA Details</h3>
+        <h4 class="font-semibold text-lg mb-5">Add FOAPA Details</h4>
+        <h4 class="font-normal cursor-pointer underline text-sm mt-0 text-gray-700">Need help? Click here to learn more
+          about FOAPA
+          and how to use them.</h4>
+        <Form @submit="addFoapa">
+          <div class="mt-7 flex flex-wrap gap-8">
+            <div class="flex flex-col gap-y-3">
+              <label for="foapa-name" class="text-sm">FOAPA Name</label>
+              <Field name="foapa-name" placeholder="Name"
+                class="text-xs border-box px-4 rounded-md border border-gray-100 shadow-md h-9 border-solid w-40">
+              </Field>
+              <ErrorMessage name="foapa-name" class="text-red-400 text-xs"></ErrorMessage>
+            </div>
+            <div class="flex flex-col gap-y-3">
+              <label for="foapa-description" class="text-sm">FOAPA Description</label>
+              <Field name="foapa-description" placeholder="Description"
+                class="text-xs border-box px-4 rounded-md border border-gray-100 shadow-md h-9 border-solid w-40">
+              </Field>
+              <ErrorMessage name="foapa-description" class="text-red-400 text-xs"></ErrorMessage>
+            </div>
+            <div class="flex flex-col gap-y-3">
+              <label for="initial-amount" class="text-sm">Initial/Current Amount</label>
+              <Field name="initial-amount" placeholder="Enter Initial Amount"
+                class="text-xs border-box px-4 rounded-md border border-gray-100 shadow-md h-9 border-solid w-40">
+              </Field>
+              <ErrorMessage name="initial-amount" class="text-red-400 text-xs"></ErrorMessage>
+            </div>
+          </div>
+
+          <!-- SECOND ROW -->
+
+          <div class="mt-8 flex flex-wrap gap-8">
+            <div class="flex flex-col gap-y-3">
+              <label for="fund" class="text-sm">FUND</label>
+              <Field name="fund" placeholder="Enter FUND"
+                class="text-xs border-box px-4 rounded-md border border-gray-100 shadow-md h-9 border-solid w-28">
+              </Field>
+              <ErrorMessage name="fund" class="text-red-400 text-xs"></ErrorMessage>
+            </div>
+            <div class="flex flex-col gap-y-3">
+              <label for="organization" class="text-sm">ORGANIZATION (ORG)</label>
+              <Field name="organization" placeholder="Enter ORG"
+                class="text-xs border-box px-4 rounded-md border border-gray-100 shadow-md h-9 border-solid w-28">
+              </Field>
+              <ErrorMessage name="organization" class="text-red-400 text-xs"></ErrorMessage>
+            </div>
+            <div class="flex flex-col gap-y-3">
+              <label for="account" class="text-sm">ACCOUNT (ACCT)</label>
+              <Field name="account" placeholder="Enter ACCT"
+                class="text-xs border-box px-4 rounded-md border border-gray-100 shadow-md h-9 border-solid w-28">
+              </Field>
+              <ErrorMessage name="account" class="text-red-400 text-xs"></ErrorMessage>
+            </div>
+            <div class="flex flex-col gap-y-3">
+              <label for="program" class="text-sm">PROGRAM (PROG)</label>
+              <Field name="program" placeholder="Enter PROG"
+                class="text-xs border-box px-4 rounded-md border border-gray-100 shadow-md h-9 border-solid w-28">
+              </Field>
+              <ErrorMessage name="program" class="text-red-400 text-xs"></ErrorMessage>
+            </div>
+            <div class="flex flex-col gap-y-3">
+              <label for="activity" class="text-sm">ACTIVITY (ACTV)</label>
+              <Field name="activity" placeholder="Enter ACTV"
+                class="text-xs border-box px-4 rounded-md border border-gray-100 shadow-md h-9 border-solid w-28">
+              </Field>
+              <ErrorMessage name="activity" class="text-red-400 text-xs"></ErrorMessage>
+            </div>
+          </div>
+          <button type="submit"
+            class=" bg-udmercy-blue mt-10 text-white border-none w-40 h-11 rounded-full cursor-pointer text-xs">Add
+            FOAPA
+          </button>
+        </Form>
+      </section>
+      <section>
+        <h3 class="mt-28 font-semibold text-3xl">Your FOAPA Details</h3>
+        <div class="border shadow-sm w-[30rem] box-border px-7 py-6 border-gray-200 border-solid h-auto">
+          <h3 class="font-semibold text-base mt-0">UDMP: 3293-2323-XXXXXX-3232</h3>
+          <h4 class="text-sm font-normal">This is a description</h4>
+          <h4 class="text-sm font-normal">Current amount: $5000</h4>
+          <div class="flex gap-5 items-center">
+            <img :src="EditIcon" alt="Edit icon" class="w-5 cursor-pointer">
+            <img :src="DeleteIcon" alt="Trash icon" class="w-4 cursor-pointer">
+            <span class="flex items-center gap-3 hover:underline">
+              <!-- <img :src="ViewIcon" alt="View icon" class="w-5 cursor-pointer"> -->
+              <h4 class="text-xs m-0 font-normal cursor-pointer">View more information</h4>
+            </span>
+          </div>
+        </div>
+      </section>
+    </section>
+
     <ConfirmationPopup :continue-function="returnToDashboard" :cancel-function="stayOnPage"
       v-show="show_leave_dialogue">
       <template #message>
@@ -27,20 +112,27 @@
 
 <script lang="ts" setup>
 import ConfirmationPopup from "../components/utilities/ConfirmationPopup.vue";
-import ManageFoapaDetails from "../components/manage-foapa/ManageFoapaDetails.vue";
+import { Form, Field, ErrorMessage } from "vee-validate";
 import axios from "axios";
+import EditIcon from "../assets/blue-pencil.png"
+import DeleteIcon from "../assets/red-delete-icon.png"
+import ViewIcon from "../assets/gray-eye-icon.png"
 import { ref, onMounted } from "vue";
 import { FoapaStuff } from "../types/types";
 import { onBeforeRouteLeave, useRouter } from "vue-router";
 import { TYPE, useToast } from "vue-toastification";
+import { isValidFoapaName } from "../utils/validators";
 
 const router = useRouter();
 let foapaDetails = ref<FoapaStuff[]>([]);
 let changes_were_made = ref<boolean>(false)
 let show_leave_dialogue = ref<boolean>(false)
 
-
 const toast = useToast()
+
+function addFoapa() {
+
+}
 
 function changesWereMade() {
   changes_were_made.value = true
