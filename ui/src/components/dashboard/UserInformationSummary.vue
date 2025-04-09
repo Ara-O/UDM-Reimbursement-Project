3293f19732c481e8489fa83959de4e0b1f64024e
Ara-O <oladipoeyiara@gmail.com>
2025-04-08 23:02:43 -0400

    adding mini menu

--- ui/src/components/dashboard/UserInformationSummary.vue
+++ ui/src/components/dashboard/UserInformationSummary.vue
@@ -1,6 +1,6 @@
 <template>
   <h3 class="text-base font-semibold mt-5">User Information</h3>
-  <div class="flex items-center justify-start gap-5 mt-7">
+  <div class="flex items-center justify-start gap-5 mt-7 relative">
     <img :src="DetroitMercyLogo" alt="Detroit Mercy Logo" class="w-16" />
     <span class="flex flex-col gap-1">
       <skeleton v-if="!user_information_has_loaded" height="20px"></skeleton>
@@ -31,15 +31,48 @@
         </p>
       </span>
     </span>
+    <div>
+      <img
+        :src="ThreeDots"
+        @click="toggleMenu"
+        alt="Three dots"
+        class="w-5 opacity-50 absolute top-3.5 right-4 cursor-pointer"
+      />
+
+      <Popover ref="menu_ref">
+        <div class="flex flex-col gap-4">
+          <div>
+            <span class="font-medium text-[14.5px] block underline mb-2"
+              >Menu</span
+            >
+            <router-link to="/account" class="text-inherit no-underline">
+              <p class="text-[13px] hover:underline cursor-pointer">
+                View Archived Requests
+              </p>
+            </router-link>
+            <router-link
+              to="/change-password"
+              class="text-inherit no-underline"
+            >
+              <p class="text-[13px] hover:underline cursor-pointer">
+                Change Password
+              </p>
+            </router-link>
+          </div>
+        </div>
+      </Popover>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { inject, ref, watch } from "vue";
+import ThreeDots from "../../assets/three-dots.png";
 import DetroitMercyLogo from "../../assets/detroit-mercy-logo.png";
 import Skeleton from "primevue/skeleton";
 import { UserInformationSummary } from "../../types/types";
 import { useRouter } from "vue-router";
+import Popover from "primevue/popover";
 import { useToast, TYPE } from "vue-toastification";
 
 const user_information_has_loaded = ref<boolean>(false);
@@ -50,6 +83,7 @@ const user_information_data = inject<null | UserInformationSummary>(
 
 const router = useRouter();
 const toast = useToast();
+const menu_ref = ref();
 
 watch(user_information_data as object, (newValue) => {
   user_information_has_loaded.value = true;
@@ -61,5 +95,9 @@ function signOut() {
   toast("Successfully signed out!", {
     type: TYPE.SUCCESS,
   });
+}
+
+function toggleMenu(event) {
+  menu_ref.value.toggle(event);
 }
 </script>
