e1107c882c6ab17029128a2b527a89e0a0a22867
Ara-O <oladipoeyiara@gmail.com>
2025-04-16 14:43:47 -0400

    adding vercel stats

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,21 +1,26 @@
 <template>
+  <Analytics />
   <router-view></router-view>
 </template>
 
 <script lang="ts" setup>
-import axios from 'axios';
-import { onMounted } from 'vue';
-import { useRouter } from 'vue-router';
+import { Analytics } from "@vercel/analytics/vue";
+import axios from "axios";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
 
-const router = useRouter()
+const router = useRouter();
 
 onMounted(() => {
-  if (localStorage.getItem("token") !== null && (localStorage.getItem("token")?.length ?? 0 > 0)) {
+  if (
+    localStorage.getItem("token") !== null &&
+    (localStorage.getItem("token")?.length ?? 0 > 0)
+  ) {
     axios.defaults.headers.common["authorization"] =
       localStorage.getItem("token");
     // console.log("User is logged in")
   } else {
     // console.log("User is not logged in");
   }
-})
+});
 </script>
